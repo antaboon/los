0c5cda5571bbaf5a16f792ca5a2063aca1631171
Ali Rizwan <f2015-330@bnu.edu.pk>
2018-11-29 21:24:10 +0500

    Added the inital project

---
+++ Line of Sight/Assets/Camerafollow.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothspeed = 0.125f;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredposition = target.position + offset;
+        transform.position = desiredposition;
+    }
+}
