c8a101721112df5e3eee809e4b3c1409ddb473af
Hisham Tajik <hishamtajik1992@gmail.com>
2018-12-17 21:08:05 +0500

    Updated with scripted enemies and reload

---
+++ Line of Sight/Assets/Scripts/EnemyScript.js
@@ -0,0 +1,11 @@
+var EnemyHealth : int = 10;
+
+function DeductPoints (DamageAmount : int) {
+	EnemyHealth -= DamageAmount;
+}
+
+function Update () {
+	if (EnemyHealth <= 0) {
+		Destroy(gameObject);
+	}
+}
---
+++ Line of Sight/Assets/Scripts/GunFire.js
@@ -0,0 +1,12 @@
+function Update () {
+if (GlobalAmmo.LoadedAmmo >= 1)
+{
+
+	if(Input.GetButtonDown("Fire1")) {
+		var gunsound : AudioSource = GetComponent.<AudioSource>();
+		gunsound.Play();
+		GetComponent.<Animation>().Play("GunShot");
+        GlobalAmmo.LoadedAmmo -= 1;
+	}
+}
+}
