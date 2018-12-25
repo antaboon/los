128202d14e11d0bd18fc5d6c090eaaa60efa0d71
lordkuface <kuku110596@gmail.com>
2018-12-25 17:59:51 +0500

    Added matchmaking

---
+++ Line of Sight/Assets/HostGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class HostGame : MonoBehaviour {
+    [SerializeField]
+    private uint roomSize = 10;
+
+    private string roomName;
+
+    public void SetRoomName (string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoom ()
+    {
+        if (roomName != "" && roomName != null) {
+            Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " players");
+        }
+    }
+}
